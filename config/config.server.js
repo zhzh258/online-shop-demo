d99787e9a295c5884d0df6504beaae019f8c75e9
nonox <nonoxochitl@outlook.com>
2023-07-05 21:39:53 +0800

    demo azure

--- config/config.server.js
+++ config/config.server.js
@@ -1,6 +1,6 @@
-const url = "http://online-shop-azure.azurewebsites.net"
-// const url = "http://127.0.0.1"
-const port = 3000;
+// const url = "http://online-shop-azure.azurewebsites.net"
+const url = "http://127.0.0.1"
+const port = 8080;
 
 module.exports = {
     url: url,
